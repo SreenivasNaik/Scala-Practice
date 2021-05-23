b9ea1d1a17a123034edb3eb35fd4ebd6afb1cb55
Sreenivas Naik <84576635+SreenivasNaik@users.noreply.github.com>
2021-05-23 10:16:16 +0530

    Scala Objects and Class
    Scala Objects are Singleton - for class level functionality  
    Classes are for instance level functionality

--- src/lectures/part2OOP/Objects.scala
+++ src/lectures/part2OOP/Objects.scala
@@ -4,17 +4,29 @@ object Objects extends App {
 
   //SCALA DOES NOT HAVE CLASS LEVEL FUNCTIONALITY - STATIC IS NOT PRESENT
   object Person{  // Type + its only Instance
+    // Static or Class level functionality
     val N_EYES = 2;
     def canFly:Boolean = false
+    //factory method
+  //  def from(mother:Person,father:Person):Person = new Person("Baby")
+    def apply(mother:Person,father:Person):Person = new Person("Baby")
   }
 
-  println(Person.N_EYES)
+  class Person(val name:String){
+    // Instance level functionality
+
+  }
+  // COMPANIONS
+  println(Person.N_EYES) // Class Level Settings
   println(Person.canFly)
 
   // Scala objects are Singletons
 
-  val mary = Person
-  val ja = Person
+
+  val mary = new Person("Marry")
+  val ja = new Person("Jhon")
   println(mary == ja)
 
+  //val baby = Person.from(mary,ja)
+  val baby = Person(mary,ja)
 }
